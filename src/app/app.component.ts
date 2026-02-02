8d79a1107664c09087a05f4e040d7557bd084059
Alexsander Campos <ext-alexsander.boas@bbts.com.br>
2026-02-02 17:12:19 -0300

    Diretivas @if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DireIfComponent } from './diretivas/dire-if/dire-if.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DireIfComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/diretivas/dire-if/dire-if.component.ts
+++ src/app/diretivas/dire-if/dire-if.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dire-if',
   standalone: true,
   imports: [],
   templateUrl: './dire-if.component.html',
-  styleUrl: './dire-if.component.css'
+  styleUrl: './dire-if.component.css',
 })
-export class DireIfComponent {
+export class DireIfComponent implements OnInit {
+  cursos: string[] = ['Angular.js'];
 
+  mostrarCursos: boolean = true;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  changeButton(): void {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
